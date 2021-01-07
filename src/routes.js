7776aa6578047958256afc85a513dcb41df7ccb4
Juan Manuel Hébert <juanhebert@protonmail.com>
2021-01-07 20:34:07 +0100

    Add auto camel-casing to pg-promise config

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,28 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const path = require('path');
+const humps = require('humps');
+
+const options = {
+  receive: data => {
+    const template = data[0];
+    template.forEach(prop => {
+      const camel = humps.camelize(prop);
+      if (!(camel in template)) {
+        for (let i = 0; i < data.length; i += 1) {
+          const d = data[i];
+          d[camel] = d[prop];
+          delete d[prop];
+        }
+      }
+    });
+  },
+};
+
+const pgp = require('pg-promise')(options);
+
+const cn = process.env.DATABASE_URL || 'postgres://coffeebrain:coffeebrain-local@localhost:5432/coffeebrain';
+const db = pgp(cn);
 
 const app = express();
 app.use(bodyParser.json());
