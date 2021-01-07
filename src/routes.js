423181ac5cb80aeeaab44f4e5032cdd821fe2fa7
Juan Manuel Hébert <juanhebert@protonmail.com>
2021-01-07 21:30:34 +0100

    Add MVP POST routes

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,17 @@ const {
   getActorInventory,
   getActorOwnership,
   getActorPractices,
+  newTransformation,
+  newTransformationInput,
+  newTransformationOutput,
+  newProduct,
+  newShipment,
+  newShipmentInput,
+  newSale,
+  newSaleInput,
+  newCertificate,
+  newPractice,
+  newAttachment,
 } = require('./database/queries');
 
 const cn = process.env.DATABASE_URL || 'postgres://coffeechain:coffeechain-local@localhost:5432/coffeechain';
@@ -48,6 +59,55 @@ app.get('/api/ownership/:actor', async (req, res) => {
 app.get('/api/practices/:actor', async (req, res) => {
   const { actor } = req.params;
   res.send({ items: await db.any(getActorPractices, [actor]) });
+});
+
+app.post('/api/transform', async (req, res) => {
+  const { emitter, inputs, outputs, timestamp } = req.body;
+  const id = timestamp; // TODO: generate randomly
+  await db.none(newTransformation, [id, emitter, timestamp]);
+  await Promise.all(inputs.map(({ productId }) => db.none(newTransformationInput, [id, productId])));
+  await Promise.all(
+    outputs.map(async ({ productId, weight, type }) => {
+      await db.none(newProduct, [productId, weight, type, null]);
+      await db.none(newTransformationOutput, [id, productId]);
+    }),
+  );
+  res.send('OK');
+});
+
+app.post('/api/ship', async (req, res) => {
+  const { sender, recipient, inputs, timestamp } = req.body;
+  const id = timestamp;
+  await db.none(newShipment, [id, sender, recipient, timestamp]);
+  await Promise.all(inputs.map(({ productId }) => db.none(newShipmentInput, [id, productId])));
+  res.send('OK');
+});
+
+app.post('/api/sell', async (req, res) => {
+  const { seller, buyer, inputs, price, currency, timestamp } = req.body;
+  const id = timestamp;
+  await db.none(newSale, [id, seller, buyer, price, currency, timestamp]);
+  await Promise.all(inputs.map(({ productId }) => db.none(newSaleInput, [id, productId])));
+  res.send('OK');
+});
+
+app.post('/api/certificate', async (req, res) => {
+  const { emitter, receiver, type, beginning, expiration } = req.body;
+  await db.none(newCertificate, [emitter, receiver, type, beginning, expiration]);
+  res.send('OK');
+});
+
+app.post('/api/practice', async (req, res) => {
+  const { emitter, receiver, type, timestamp } = req.body;
+  await db.none(newPractice, [emitter, receiver, type, timestamp]);
+  res.send('OK');
+});
+
+app.post('/api/textAttachment', async (req, res) => {
+  const { eventId, eventType, content, emitter } = req.body;
+  // TODO: generate id, add timestamp to database schema for attachments
+  await db.none(newAttachment, ['id', eventId, eventType, 'TEXT', content, emitter]);
+  res.send('OK');
 });
 
 if (process.env.NODE_ENV === 'production') {
