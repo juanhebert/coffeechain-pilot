3b8e1754c1510cd7fc648adb373ec811a012a067
Juan Manuel Hébert <juanhebert@protonmail.com>
2021-03-03 08:09:06 +0100

    Add actors in migration script

--- migration.js
+++ migration.js
@@ -1,21 +1,55 @@
 /* eslint-disable no-console */
 const yargs = require('yargs');
 const pgPromise = require('pg-promise');
+const short = require('short-uuid');
 
 const cn = process.env.DATABASE_URL || 'postgres://coffeechain:coffeechain-local@localhost:5432/coffeechain';
 const pgp = pgPromise();
 const db = pgp(cn);
 
-const { resetSchema } = require('./src/database/queries');
+const { resetSchema, newActor } = require('./src/database/queries');
 
-const { argv } = yargs.alias('r', 'reset');
+const { argv } = yargs.alias('r', 'reset').alias('a', 'actors');
+
+const actors = [
+  {
+    name: 'Ernesto Cárdenas',
+    location: 'Titiribí, Colombia',
+    type: 'FARMER',
+  },
+  {
+    name: 'Cooperativa de Caficultores de Antioquia',
+    location: 'Medellín, Colombia',
+    type: 'COOPERATIVE',
+  },
+  {
+    name: 'Almacafé',
+    location: 'Medellín, Colombia',
+    type: 'DRY_MILL',
+  },
+  {
+    name: 'Löfberg',
+    location: 'Karlstad, Sweden',
+    type: 'ROASTER',
+  },
+];
 
 const runMigration = async () => {
   const { reset } = argv;
 
-  if (reset) {
+  if (reset || actors) {
     console.log('Wiping the database...');
     await db.none(resetSchema);
+  }
+
+  if (actors) {
+    console.log('Adding actors to the database...');
+    await Promise.all(
+      actors.map(async ({ name, location, type }) => {
+        const id = short.generate();
+        await db.none(newActor, [id, name, location, null, type]);
+      }),
+    );
   }
 };
 
